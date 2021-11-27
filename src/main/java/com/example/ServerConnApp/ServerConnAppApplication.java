6fee3aed03ec4d87b8b8182d8ca83448c069deec
hachanghyun <haccwx@naver.com>
2021-11-27 10:31:34 +0900

    Initial commit

---
+++ src/main/java/com/example/ServerConnApp/ServerConnAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ServerConnApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerConnAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerConnAppApplication.class, args);
+	}
+
+}
